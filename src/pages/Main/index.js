ccfb0b118edae54005684ff3698536a9b584d8b7
Bruno Jutkoski <bjutkoski@MacBook-Pro-de-Bruno.local>
2019-10-25 00:58:09 -0300

    Buscando dados da API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -34,7 +34,7 @@ export default function Main({ navigation }) {
       }
     }
     getItem();
-  }, []);
+  }, [setUsers]);
 
   useEffect(() => {
     AsyncStorage.setItem('users', JSON.stringify(users));
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+  const [stars, setStars] = useState([]);
+
+  useEffect(() => {
+    async function getStars() {
+      const user = navigation.getParam('user');
+
+      const response = await api.get(`/users/${user.login}/starred`);
+      setStars(response.data);
+    }
+    getStars();
+  }, [navigation, setStars]);
+
   return <View />;
 }
+
+User.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('user').name,
+});
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
